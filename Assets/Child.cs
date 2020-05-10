efcafc9deb8dcb63ef05ff74b3d01034bf41133f
Jordan Lanius <jwlq89@mail.umsl.edu>
2020-05-10 16:31:34 -0500

    Children Wander
    Children now occasionally wander randomly.
    Attractor changed to a cupcake.
    Walls prevent children from leaving room.

--- Assets/Child.cs
+++ Assets/Child.cs
@@ -6,35 +6,65 @@ using static UnityEngine.Random;
 public class Child : MonoBehaviour
 {
 
-    public int speed;
+    public int speed, decisions, counter;
+    public float attraction;
 
     public bool move = false;
 
     private Transform target;
+    private Vector2 current;
 
 
     // Start is called before the first frame update
     void Awake()
     {
         target = GameObject.FindGameObjectWithTag("attractor").GetComponent<Transform>();
-        speed = Random.Range(1, 6);
+        speed = Random.Range(3, 6);
+        decisions = Random.Range(300, 500);
+        attraction = Random.Range((float).4, (float).8);
+        counter = 0;
     }
-
     
     public void SetMove(bool mov)
     {
         move = mov;
     }
+
     // Update is called once per frame
     void Update()
     {
+        float distance;
+
+        Vector2 movement = movement = new Vector2(
+            transform.position.x + (Random.Range(-1, 2) * 1000),
+            transform.position.y + (Random.Range(-1, 2) * 1000));
+
         if (move){
-            
-            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime); 
+            if (counter % decisions != 0)
+            {
+                movement = current;
+            }
+            else if ((Random.Range((float)0, (float)1) <= attraction) && (counter >= 3 * decisions))
+            {
+                movement = target.position;
+            }
+
+            distance = speed * Time.deltaTime;
         }
-       else {
-            transform.position = Vector2.MoveTowards(transform.position, target.position, 0 * Time.deltaTime);
+        else
+        {
+            movement = transform.position;
+            distance = 0 * Time.deltaTime;
 	    }
+
+        transform.position = Vector2.MoveTowards(transform.position, movement, distance);
+        current = movement;
+
+        counter++;
+        if (counter == 1000 * decisions)
+        {
+            counter = 3 * decisions;
+        }
     }
 
 
